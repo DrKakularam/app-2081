d69c25ddf48b85821e5ec8437e22b73a27a71eab
Kakularam <kumar.kakularam@gmail.com>
2025-08-22 09:41:23 +0200

    project created

---
+++ pages/page2.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+import numpy as np
+import pandas as pd
+
+st.markdown("""# This is a header
+## This is a sub header
+This is text""")
+
+df = pd.DataFrame({
+    'first column': list(range(1, 11)),
+    'second column': np.arange(10, 101, 10)
+})
+
+number = st.number_input('Insert a number')
+
+st.write('The current number is ', number)
+
+
+# this slider allows the user to select a number of lines
+# to display in the dataframe
+# the selected value is returned by st.slider
+line_count = st.slider('Select a line count', 1, 10, 3)
+
+# and used to select the displayed lines
+head_df = df.head(line_count)
+
+head_df
